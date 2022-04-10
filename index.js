553b51a874c5e4a6515a9be32e6834371fdfbbdb
Vanja Andrejic <njavagg@gmail.com>
2022-04-10 21:09:28 +0200

    Group Stage

--- index.js
+++ index.js
@@ -49,13 +49,12 @@ const main = () => {
     ],
 
     'Grupa H': [
-      { ime: 'Portugal', rang: 8 , poeni: 0, datiGolovi: 0, primljeniGolovi: 0, pobede: 0, neresene: 0, porazi: 0 },
-      { ime: 'Gana', rang: 60 , poeni: 0, datiGolovi: 0, primljeniGolovi: 0, pobede: 0, neresene: 0, porazi: 0 },
-      { ime: 'Urugvaj', rang: 13 , poeni: 0, datiGolovi: 0, primljeniGolovi: 0, pobede: 0, neresene: 0, porazi: 0 },
-      { ime: 'Južna Koreja', rang: 29 , poeni: 0, datiGolovi: 0, primljeniGolovi: 0, pobede: 0, neresene: 0, porazi: 0 }
+      { ime: 'Portugal', rang: 8, poeni: 0, datiGolovi: 0, primljeniGolovi: 0, pobede: 0, neresene: 0, porazi: 0 },
+      { ime: 'Gana', rang: 60, poeni: 0, datiGolovi: 0, primljeniGolovi: 0, pobede: 0, neresene: 0, porazi: 0 },
+      { ime: 'Urugvaj', rang: 13, poeni: 0, datiGolovi: 0, primljeniGolovi: 0, pobede: 0, neresene: 0, porazi: 0 },
+      { ime: 'Južna Koreja', rang: 29, poeni: 0, datiGolovi: 0, primljeniGolovi: 0, pobede: 0, neresene: 0, porazi: 0 }
     ]
   }
-
 
   function getRandomNum(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -82,16 +81,12 @@ const main = () => {
 
     } else if (tim2.golovi > tim1.golovi) {
 
-
-
       tim1.porazi = tim1.porazi += 1;
       tim2.pobede = tim2.pobede += 1;
 
       tim2.poeni = tim2.poeni += 3;
 
     } else {
-
-
 
       tim1.neresene = tim1.neresene += 1;
       tim2.neresene = tim2.neresene += 1;
@@ -106,27 +101,33 @@ const main = () => {
   function grupnaFazaUtakmice() {
 
     console.log('Grupna faza - I kolo:');
+    console.log('');
 
     for (const grupa in grupe) {
       console.log(' ', grupa);
       utakmicaGrupa(grupe[grupa][0], grupe[grupa][1]);
       utakmicaGrupa(grupe[grupa][2], grupe[grupa][3]);
+      console.log('');
     }
 
     console.log('Grupna faza - II kolo:');
+    console.log('');
 
     for (const grupa in grupe) {
       console.log(' ', grupa);
       utakmicaGrupa(grupe[grupa][1], grupe[grupa][2]);
       utakmicaGrupa(grupe[grupa][3], grupe[grupa][0]);
+      console.log('');
     }
 
     console.log('Grupna faza - III kolo:');
+    console.log('');
 
     for (const grupa in grupe) {
       console.log(' ', grupa);
       utakmicaGrupa(grupe[grupa][1], grupe[grupa][3]);
       utakmicaGrupa(grupe[grupa][0], grupe[grupa][2]);
+      console.log('');
     }
 
   }
@@ -135,34 +136,142 @@ const main = () => {
   function konacneTabele() {
 
     console.log('Kraj grupne faze:')
-    for (const grupa in grupe) { 
+    console.log('');
 
-      grupe[grupa].sort(function(a, b) {
-          
+    for (const grupa in grupe) {
+
+      grupe[grupa].sort(function (a, b) {
+
         if (a.poeni < b.poeni) {
           return 1;
         }
         else if (a.poeni > b.poeni) {
           return -1;
         }
-        else if (a.poeni === b.poeni && Number(a.datiGolovi - a.primljeniGolovi) > Number(b.datiGolovi - b.primljeniGolovi)){
+        else if (a.poeni === b.poeni && Number(a.datiGolovi - a.primljeniGolovi) > Number(b.datiGolovi - b.primljeniGolovi)) {
           return -1;
         }
         else if (a.poeni === b.poeni && Number(a.datiGolovi - a.primljeniGolovi) < Number(b.datiGolovi - b.primljeniGolovi)) {
           return 1;
-        } else if (a.poeni === b.poeni && Number(a.datiGolovi - a.primljeniGolovi) === Number(b.datiGolovi - b.primljeniGolovi) && a.datiGolovi > b.datiGolovi){
-         return -1;
-        } if  (a.poeni === b.poeni && Number(a.datiGolovi - a.primljeniGolovi) === Number(b.datiGolovi - b.primljeniGolovi) && a.datiGolovi < b.datiGolovi) {
+        } else if (a.poeni === b.poeni && Number(a.datiGolovi - a.primljeniGolovi) === Number(b.datiGolovi - b.primljeniGolovi) && a.datiGolovi > b.datiGolovi) {
+          return -1;
+        } if (a.poeni === b.poeni && Number(a.datiGolovi - a.primljeniGolovi) === Number(b.datiGolovi - b.primljeniGolovi) && a.datiGolovi < b.datiGolovi) {
           return 1;
         }
       });
 
       console.log(' ', grupa);
-      grupe[grupa].forEach(tim => console.log('       ',tim.ime, '(' , tim.rang, ')','            ', tim.pobede, tim.neresene, tim.porazi,'   ', tim.datiGolovi,':', tim.primljeniGolovi,'   ', tim.poeni));
+
+      grupe[grupa].forEach(tim => {
+
+        const brojKaraktera = (tim.ime + tim.rang + tim.pobede + tim.neresene + tim.porazi + tim.datiGolovi + tim.primljeniGolovi + tim.poeni).length;
+        const razmak = ' ';
+        console.log('       ', tim.ime, '(', tim.rang, ')', razmak.repeat(30 - brojKaraktera), tim.pobede, tim.neresene, tim.porazi, ' ', tim.datiGolovi, ':', tim.primljeniGolovi, '   ', tim.poeni);
+
+      })
+
     }
   }
 
   konacneTabele(grupe);
+
+
+  function utakmicaNokautPobednk(tim1, tim2) {
+
+    tim1.golovi = getRandomNum(0, 5);
+    tim2.golovi = getRandomNum(0, 5);
+
+    let pobednik
+
+    if (tim1.golovi > tim2.golovi) {
+
+      pobednik = tim1
+
+    } else if (tim1.golovi < tim2.golovi) {
+
+      pobednik = tim2
+
+    } else if (tim1.golovi === tim2.golovi && tim1.rang > tim2.rang) {
+
+      tim1.golovi = tim1.golovi += 1;
+      pobednik = tim1
+
+    } else if (tim1.golovi === tim2.golovi && tim1.rang < tim2.rang) {
+
+      tim2.golovi = tim2.golovi += 1;
+      pobednik = tim2
+
+    } else {
+
+      pobednik = tim1
+
+    }
+
+    console.log('');
+    console.log('   ', tim1.ime, tim1.golovi, ':', tim2.golovi, tim2.ime);
+
+    return pobednik
+  };
+
+  const pobednik16 = [];
+  const pobednik8 = [];
+  const pobednikPolu = [];
+  const pobednikFinala = [];
+
+  function nokaut16Faza() {
+    for (const grupa in grupe) {
+      grupe[grupa].splice(-2);
+    }
+
+    console.log('');
+    console.log('1/16 Finala:')
+    pobednik16.push(utakmicaNokautPobednk(grupe['Grupa A'][0], (grupe['Grupa B'][1])));
+    pobednik16.push(utakmicaNokautPobednk(grupe['Grupa B'][0], (grupe['Grupa A'][1])));
+    pobednik16.push(utakmicaNokautPobednk(grupe['Grupa C'][0], (grupe['Grupa D'][1])));
+    pobednik16.push(utakmicaNokautPobednk(grupe['Grupa D'][0], (grupe['Grupa C'][1])));
+    pobednik16.push(utakmicaNokautPobednk(grupe['Grupa E'][0], (grupe['Grupa F'][1])));
+    pobednik16.push(utakmicaNokautPobednk(grupe['Grupa F'][0], (grupe['Grupa E'][1])));
+    pobednik16.push(utakmicaNokautPobednk(grupe['Grupa G'][0], (grupe['Grupa H'][1])));
+    pobednik16.push(utakmicaNokautPobednk(grupe['Grupa H'][0], (grupe['Grupa G'][1])));
+    console.log('');
+
+  }
+  nokaut16Faza();
+
+  function nokaut8Faza() {
+
+    console.log[''];
+    console.log('1/8 Finala:')
+    pobednik8.push(utakmicaNokautPobednk(pobednik16[0], pobednik16[1]));
+    pobednik8.push(utakmicaNokautPobednk(pobednik16[2], pobednik16[3]));
+    pobednik8.push(utakmicaNokautPobednk(pobednik16[4], pobednik16[5]));
+    pobednik8.push(utakmicaNokautPobednk(pobednik16[6], pobednik16[7]));
+    console.log('');
+
+  }
+  nokaut8Faza();
+
+  function polufinale() {
+
+    console.log[''];
+    console.log('Polufinale:')
+    pobednikPolu.push(utakmicaNokautPobednk(pobednik8[0], pobednik8[1]));
+    pobednikPolu.push(utakmicaNokautPobednk(pobednik8[2], pobednik8[3]));
+    console.log('');
+  }
+  polufinale()
+
+  function finale() {
+
+    console.log[''];
+    console.log('Finale:')
+    pobednikFinala.push(utakmicaNokautPobednk(pobednikPolu[0], pobednikPolu[1]));
+    console.log('');
+    console.log('Pobednik:')
+    console.log('!!!', pobednikFinala[0].ime.toUpperCase(), '!!!');
+    console.log('');
+  }
+  finale();
 
 }
 main();
